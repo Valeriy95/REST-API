b6c5e0ca6d765457a88c2e339d7111534a46de07
Valeriy95 <vmariukhnyk@gmail.com>
2025-02-11 11:52:26 +0300

    feat: init

---
+++ my-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AuthorSelector from './components/AuthorSelector'
+
+function App() {
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Author Selector</h1>
+      <AuthorSelector />
+    </div>
+  )
+}
+
+export default App
---
+++ my-project/src/components/AuthorSelector.tsx
@@ -0,0 +1,5 @@
+const AuthorSelector: React.FC = () => {
+
+};
+
+export default AuthorSelector;
